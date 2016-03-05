75456f5d9203c04d0fbc8c4ac06b7cf05c800f3e
Amir El Bawab <amirbawab@gmail.com>
2016-03-05 16:44:37 -0500

    Added getter methods

--- Game.h
+++ Game.h
@@ -15,20 +15,17 @@ class Game {
 private:
 
 	// General attributes
-	int numTurns;  // the number of turns since the beginning
+	int fullTurn;  // the number of turns since the beginning
 	std::vector<std::shared_ptr<Player>> players;
 	std::shared_ptr<ResourceMarket> rMarket;
 	CardStack cardStack;
 	std::vector<std::shared_ptr<Card>> allCards;
 	std::shared_ptr<Map> map;
-
 	std::vector<std::shared_ptr<Player>> playerOrder;  // players placed in the correct turn order
 	int playStep;  // current step of the turn
 	std::shared_ptr<Player> currentPlayer;  // pointer to the current player
 	int phase;  // phase of the game (1, 2 or 3)
 	bool gameOver = false;
-
-	// For step 2
 	int currentBid;
 	std::shared_ptr<Player> highestBidder;
 
@@ -39,7 +36,20 @@ public:
 	void Setup();
 	void PlayGame();
 
-	
+	// Getters
+	int GetFullTurn() const { return fullTurn; }
+	std::vector<std::shared_ptr<Player>>& GetPlayers() { return players; }
+	std::shared_ptr<ResourceMarket>& GetResourceMarket() { return rMarket; }
+	CardStack& GetCardStack() { return cardStack; }
+	std::vector<std::shared_ptr<Card>>& GetAllCards() { return allCards; }
+	std::shared_ptr<Map>& GetMap() { return map; }
+	std::vector<std::shared_ptr<Player>>& GetPlayerOrder() { return playerOrder; }
+	int GetPhase() { return phase; }
+
+	// Setters
+	void SetMap(std::shared_ptr<Map>& map) { this->map = map; }
+	void SetFullTurn(int fullTurn) { this->fullTurn = fullTurn; }
+	void SetPhase(int phase) { this->phase = phase; }
 
 	void UpdatePlayOrder(bool); // step 1
 	void AuctionPlants();  // step 2
